21197e82642378effab8331a7c27e03d7e26b4cd
Niall Beard <niallbeard@gmail.com>
2015-04-18 00:24:53 +0100

    Added proper messages

--- index.js
+++ index.js
@@ -9,11 +9,22 @@ $( document ).ready(function(){
                    "&r=json"].join('');
         var res = jQuery.getJSON(url, function( data ){
             console.log(data);
-            console.log(data.Rating);
-            var score = .5-((data.imdbRating*10)-data.Metascore)/200
-            console.log(score);
-            $('#meter').attr('value', score);
+            if (data.Response === 'False'){
+                $('#message').text(data.Error);
+                $('#message').attr('text-color', 'red');
+            } else {
+                console.log(data.Rating);
+                var score = .5-((data.imdbRating*10)-data.Metascore)/200
+                $('#meter').attr('value', score);
+                switch(true){
+                    case (score > 0.5):
+                        $('#message').text('Films probably quite pretencious. (' + score + ')');
+                        break;
+                    case( score <= 0.5):
+                        $('#message').text('Films probably quite dumb. (' + score + ')');
+                        break;
+                };
+            };
         });
     });
 });
-
